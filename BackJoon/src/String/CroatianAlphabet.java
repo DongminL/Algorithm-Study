e2b941c32f135b559872d030a4751df5c031b446
DongminL <milkskfk12@gmail.com>
2022-07-14 13:57:12 +0900

    2941

---
+++ BackJoon/src/String/CroatianAlphabet.java
@@ -0,0 +1,48 @@
+// [BackJoon] 2941번 : 크로아티아 알파벳
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CroatianAlphabet {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String croatian = br.readLine();
+        int count = croatian.length();
+
+        for (int i = 0; i < croatian.length(); i++) {
+            if (croatian.charAt(i) == 'j' && i >= 1) {
+                if (croatian.charAt(i-1) == 'n')
+                    count--;
+                if (croatian.charAt(i-1) == 'l')
+                    count--;
+            }
+
+            else if (croatian.charAt(i) == '=' && i >= 1) {
+                if (croatian.charAt(i-1) == 'z')
+                    count--;
+                if (croatian.charAt(i-1) == 'c')
+                    count--;
+                if (croatian.charAt(i-1) == 's')
+                    count--;
+                else if (croatian.charAt(i-1) == 'z' && i >= 2)
+                    if (croatian.charAt(i-2) == 'd')
+                        count--;
+            }
+
+            else if (croatian.charAt(i) == '-' && i >= 1) {
+                if (croatian.charAt(i-1) == 'd')
+                    count--;
+                if (croatian.charAt(i-1) == 'c')
+                    count--;
+            }
+        }
+
+        System.out.println(count);
+
+        br.close();
+    }
+}
