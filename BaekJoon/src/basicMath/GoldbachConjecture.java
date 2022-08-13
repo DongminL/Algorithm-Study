04c4da571cc399f5db1c07ffb9b9d74adb670fc8
DongminL <milkskfk12@gmail.com>
2022-08-14 02:49:33 +0900

    9020

---
+++ BaekJoon/src/basicMath/GoldbachConjecture.java
@@ -0,0 +1,41 @@
+// [BackJoon] 9020번 : 골드바흐의 추측
+package basicMath;
+
+import java.io.*;
+
+public class GoldbachConjecture {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        while (n > 0) {
+            int evenNum = Integer.parseInt(br.readLine());
+
+            boolean[] nums = new boolean[evenNum - 1];
+            nums[0] = nums[1] = true;
+
+            for (int i = 2; i <= Math.sqrt(nums.length - 1); i++) {
+                if (nums[i] == true) continue;
+
+                for (int j = i * i; j < nums.length; j += i)
+                    nums[j] = true;
+            }
+
+            for (int i = evenNum / 2; i > 1; i--) {
+                if (nums[i] == false && nums[evenNum - i] == false) {
+                    bw.write(i + " " + (evenNum - i) + "\n");
+                    break;
+                }
+            }
+
+            n--;
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
